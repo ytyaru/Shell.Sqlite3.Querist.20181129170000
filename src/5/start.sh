d8b8ed21a32f614e00e57e3efeed07af7e45b8f0
ytyaru <ytyaru@outlook.jp>
2018-11-29 21:59:04 +0900

    できた

---
+++ src/5/start.sh
@@ -0,0 +1,60 @@
+dbpath=TestDb.sqlite3
+
+tablename=TestTable
+
+InputDir=`pwd`
+OutputDir=`pwd`
+while getopts "i:o:" OPT
+do
+  case $OPT in
+    i) InputDir="$OPTARG"; echo $InputDir; ;;
+    o) OutputDir="$OPTARG"; echo $InputDir; ;;
+    h) echo  "Help";;
+  esac
+done
+
+# ディレクトリ名一覧
+# $1: 一覧したいディレクトリパス
+function DirNames() {
+    local path=`pwd`
+    cd "${1}"
+    local names=`echo $(ls -d */) | sed 's/\///g'`
+    cd $path
+    echo $names
+}
+
+# テーブル存在確認
+# $1: 存在を確認したいテーブル名
+# return: 0:存在しない, 1:存在する
+function IsExistTable() {
+    echo "tables: "`$(sqlite3 ${dbpath} ".tables")`
+    for tblnm in $(sqlite3 ${dbpath} ".tables"); do
+        echo "  ${tblnm}"
+        [ $1 = $tblnm ] && return 1
+    done
+    return 0;
+}
+
+# テーブル作成
+# $1: 作成するテーブル名
+function CreateTable() {
+    IsExistTable ${1}
+    local IsExist=$?
+    echo "IsExistTable: ${IsExist}"
+    [ 0 -lt $IsExist ] && { echo "テーブルが既存のため処理を中断します。"; exit 1; }
+
+    sqlite3 ${dbpath} < ${1}.sql
+    sqlite3 ${dbpath} ".mode tabs" ".import ./${1}.tsv ${1}"
+    sqlite3 ${dbpath} "select * from ${1};"
+}
+
+echo `pwd`
+dirnames=`DirNames "${InputDir}"`
+echo $dirnames
+echo `pwd`
+
+# 実行するとエラー: TestTable.sql: そのようなファイルやディレクトリはありません
+# 相対パスなせいで参照できないのが原因。このスクリプトの引数で入出力ディレクトリパスを渡してもらう必要がある。
+# $ start -i 入力元パス -o 出力先パス
+CreateTable ${tablename}
+
---
+++ src/6/start.sh
@@ -0,0 +1,82 @@
+# SQLiteファイル作成スクリプト
+# $ start -i /tmp/DatabasesDefine -o /tmp/DatabaseOutput
+# -i: DBディレクトリとsqlファイルが存在するルートディレクトリパス
+# -o: .sqlite3ファイルを出力するディレクトリパス
+
+InputDir=`pwd`
+OutputDir=`pwd`
+while getopts "i:o:" OPT
+do
+  case $OPT in
+    i) InputDir="$OPTARG"; echo "${InputDir}"; ;;
+    o) OutputDir="$OPTARG"; echo "${OutputDir}"; ;;
+    h) echo  "Help";;
+  esac
+done
+
+# ディレクトリ名一覧
+# $1: 一覧したいディレクトリパス
+# http://8ttyan.hatenablog.com/entry/2016/01/25/162456
+# http://yuyunko.hatenablog.com/entry/20110304/1299218939
+function DirNames() {
+    local path=`pwd`
+    cd "${1}"
+    local names=`echo $(ls -d */) | sed 's/\///g'`
+    cd $path
+    echo $names
+}
+
+# Sqlite3ファイル存在確認
+# $1: テーブル名
+function IsExistDbFile() {
+    [ -f "${OutputDir%/}/${1}" ] && return 1 || return 0
+}
+
+# テーブル存在確認
+# $1: 存在を確認したいテーブルがあるDBファイルパス
+# $1: 存在を確認したいテーブル名
+# return: 0:存在しない, 1:存在する
+function IsExistTable() {
+    echo "tables: "
+    for tblnm in $(sqlite3 "${1}" ".tables"); do
+        echo "  ${tblnm}"
+        [ "${2}" = "${tblnm}" ] && return 1
+    done
+    return 0;
+}
+
+# テーブル作成
+function CreateTable() {
+    local currentpath=`pwd`
+    echo "databases: "
+    # データベース名一覧取得
+    for dbnm in $(DirNames "${InputDir}"); do
+        echo "  ${dbnm}"
+        local dbfilename="${dbnm}.sqlite3"
+        local dbpath="${OutputDir%/}/${dbfilename}"
+        cd "${InputDir%/}/${dbnm}"
+        # SQLファイル名一覧取得
+        for flnm in $(ls -F | grep -v / | grep *.sql); do
+            local tablename=`basename ${flnm} ".sql"`
+            local sqlpath="${InputDir%/}/${dbnm}/${flnm}"
+            local tsvpath="${InputDir%/}/${dbnm}/${tablename}.tsv"
+            echo "    ${sqlpath}"
+            echo "tablename: ${tablename}"
+            echo "sqlpath: ${sqlpath}"
+            echo "tsvpath: ${tsvpath}"
+
+            IsExistTable ${dbpath} ${tablename}
+            local IsExist=$?
+            echo "IsExistTable: ${IsExist}"
+            [ 0 -lt $IsExist ] && { echo "テーブルが既存のため処理をスキップします。"; continue; }
+
+            sqlite3 ${dbpath} < "${sqlpath}"
+            sqlite3 ${dbpath} ".mode tabs" ".import \"${tsvpath}\" ${tablename}"
+            sqlite3 ${dbpath} "select * from ${tablename};"
+        done
+    done
+    cd "${currentpath}"
+}
+
+CreateTable
+
